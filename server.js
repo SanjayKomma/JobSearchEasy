08c5e0ea142e56624ffd830d7b8f209dd3f38c84
SanjayKomma <sanjukomma1@gmail.com>
2026-08-08 19:39:19 +0530

    added few things

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
 const dns = require('dns');
+const app = require("./app");
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 mongoose
     .connect(MONGODB_URI)
     .then(() =>{
         console.log("Connected to MongoDB");
+        app
+            .listen(PORT, HOST, () => {
+                console.log(`Server running on http://${HOST}:${PORT}`);
+            })
+            .on('error', (error) => {
+                console.error("Error starting server:", error.message);
+            })
     })
     .catch((error) => {
         console.log("Error connecting to MongoDB:", error.message);
