db179e7078ada9dcc140b7101336b193eb4903f5
SanjayKomma <sanjukomma1@gmail.com>
2026-08-08 23:48:43 +0530

    Login endpoint done

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const { SALT_ROUNDS } = require('../utils/config');
+const { SALT_ROUNDS, JWT_SECRET, ENV } = require('../utils/config');
+const jwt = require('jsonwebtoken');
 const authController = {
     register: async (request, response) =>{
         try{
@@ -22,12 +23,28 @@ const authController = {
             return res.status(500).json({error:error.message});
         }
     },
-    login:(request, response) =>{
+    login: async (request, response) =>{
         try{
+            const {email, password} = request.body;
+            const user = await User.findOne({ email });
+            if(!user){
+                return response.status(400).json({message:"Invalid Email or User not found"});
+            }
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if(!isPasswordValid){
+                return response.status(400).json({message:"Invalid password"});
+            }
+            const token = jwt.sign({ userId:user._id }, JWT_SECRET, { expiresIn: '1h' });
+            response.cookie('token', token, {
+                httpOnly: true,
+                secure: ENV === 'production',
+                sameSite: ENV === 'production' ? 'none' : 'lax',
+                maxAge: 3600000
+            });
             return response.status(200).json({message:"User logged in successfully"});
         }
         catch(error){
-            return res.status(500).json({error:error.message});
+            return response.status(500).json({error:error.message});
         }
     },
     getProfile:(request, response) =>{
