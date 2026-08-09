263353f77135823a0e9d3ffaf24c1ada8a729ed2
SanjayKomma <sanjukomma1@gmail.com>
2026-08-09 18:44:57 +0530

    isAuthenticated and allowRoles setup is done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authRouter = require('./routes/authRouter');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 module.exports = app;
